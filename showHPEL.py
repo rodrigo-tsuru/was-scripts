2c65687bb1f37f9f840f79a79f3ed7a6687144a9
rodrigo-tsuru <caixapostal@gmail.com>
2016-06-10 17:45:19 -0300

    alterado para mostrar as configurações de Log

--- showHPEL.py
+++ showHPEL.py
@@ -2,7 +2,9 @@ servers = "catalogServer1,catalogServer2,catalogServer3,containerServer1,contain
  
 serverArray = servers.split(",")
 for x in range(len(serverArray)):
-	print serverArray[x];
+	print "Configurações de log do servidor: " + serverArray[x];
 	HPELService = AdminConfig.getid("/Cell:DES-SSWAS-01Cell01/Node:DES-SSWAS-01Node01/Server:"+serverArray[x]+"/HighPerformanceExtensibleLogging:/");
-	print AdminConfig.showall(HPELService);
+	HPELLog = AdminConfig.list("HPELLog", HPELService)
+	print AdminConfig.showall(HPELLog);
+
 
